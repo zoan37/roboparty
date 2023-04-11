00a7746228b214cfaa62385102cdf867b37df15d
zoan37 <104385984+zoan37@users.noreply.github.com>
2023-04-11 17:34:20 -0400

    c

--- game_server.js
+++ game_server.js
@@ -15,7 +15,7 @@ function startServer() {
     const io = new Server(server, {
         cors: {
             // TODO: add origin for vercel app url
-            origin: ['http://127.0.0.1:5173', '*.vercel.app'],
+            origin: ['http://127.0.0.1:5173', 'http://robotparty-377008664.us-east-2.elb.amazonaws.com'],
             methods: ["GET", "POST"]
         }
     });
