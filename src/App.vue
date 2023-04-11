52c99ac3189aeacfcf81a7846c3049b763674e20
zoan37 <104385984+zoan37@users.noreply.github.com>
2023-04-11 18:25:37 -0400

    settings

--- src/App.vue
+++ src/App.vue
@@ -299,14 +299,56 @@ onMounted(() => {
     </div>
 
     <div id="info_area">
-      <div class="mb-3">
-        <b>Robot Companion</b> is an AI robot that can move, emote, and change facial expressions while chatting.
-      </div>
-      <div>
-        Powered by <a target="_blank" href="https://github.com/alexanderatallah/window.ai">window.ai</a> and <a
-          target="_blank" href="https://threejs.org/">three.js</a>.
-        Made by <a target="_blank" href="https://twitter.com/zoan37">zoan.eth</a>. View <a target="_blank"
-          href="https://github.com/zoan37/robot-companion">source code on GitHub</a>.
+
+    </div>
+
+    <div id="settings_area">
+      <span style="cursor: pointer;" data-bs-toggle="modal" data-bs-target="#settingsModal">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-gear"
+          viewBox="0 0 16 16">
+          <path
+            d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z" />
+          <path
+            d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z" />
+        </svg>
+      </span>
+    </div>
+  </div>
+
+  <!-- Modal -->
+  <div class="modal fade" id="settingsModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">RoboParty 🤖🎉</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="mb-3">
+            RoboParty is a 3D world where you can party with friends using a robot avatar controlled by AI.
+            It uses <a target="_blank" href="https://windowai.io/">window.ai</a>, which
+            allows you to plug in your own AI model (e.g. GPT-3). You can talk to the AI to change your username and
+            avatar color, perform different moves, emotes, and facial expressions, or have general conversations.
+          </div>
+          <div class="mb-3">
+            <b>Controls:</b> 
+            <ul>
+              <li>WASD or arrow keys to move</li>
+              <li>Click and drag mouse to look around</li>
+              <li>Talk to the AI to change username and avatar color</li>
+              <li>Talk to the AI to perform different moves, emotes, and facial expressions</li>
+            </ul>
+          </div>
+          <div>
+            Powered by <a target="_blank" href="https://windowai.io/">window.ai</a> and <a
+              target="_blank" href="https://threejs.org/">three.js</a>.
+            Made by <a target="_blank" href="https://twitter.com/zoan37">zoan.eth</a>. View <a target="_blank"
+              href="https://github.com/zoan37/roboparty">source code on GitHub</a>.
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
       </div>
     </div>
   </div>
@@ -325,6 +367,15 @@ onMounted(() => {
   background-color: transparent;
   z-index: 100;
   width: 350px;
+}
+
+#settings_area {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  padding: 20px;
+  background-color: transparent;
+  z-index: 200;
 }
 
 .user_message {
