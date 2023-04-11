c11a355a396bbbf8589f994fc4fd0e196279bd6c
zoan37 <104385984+zoan37@users.noreply.github.com>
2023-04-11 16:34:08 -0400

    origin

--- game_server.js
+++ game_server.js
@@ -15,7 +15,7 @@ function startServer() {
     const io = new Server(server, {
         cors: {
             // TODO: add origin for vercel app url
-            origin: ['http://127.0.0.1:5173'],
+            origin: ['http://127.0.0.1:5173', '*.vercel.app'],
             methods: ["GET", "POST"]
         }
     });
